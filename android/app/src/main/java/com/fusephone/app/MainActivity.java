96caddb8fb1aab3870751f12be2aef018ef20b47
FusePhone <office@fusephone.com>
2026-05-19 01:53:24 -0400

    Fix: explicitly initialize FirebaseApp in MainActivity to prevent CapacitorPlugins crash

--- android/app/src/main/java/com/fusephone/app/MainActivity.java
+++ android/app/src/main/java/com/fusephone/app/MainActivity.java
@@ -1,12 +1,27 @@
 package com.fusephone.app;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends BridgeActivity {
+    private static final String TAG = "FusePhoneMain";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        try {
+            if (FirebaseApp.getApps(this).isEmpty()) {
+                FirebaseApp.initializeApp(this);
+                Log.i(TAG, "FirebaseApp initialized explicitly in MainActivity");
+            } else {
+                Log.i(TAG, "FirebaseApp was already auto-initialized");
+            }
+        } catch (Throwable t) {
+            Log.e(TAG, "FirebaseApp.initializeApp failed", t);
+        }
+
         registerPlugin(PlayBillingPlugin.class);
         super.onCreate(savedInstanceState);
     }
